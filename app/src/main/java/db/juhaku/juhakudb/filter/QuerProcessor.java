0c613f196e5f94c90aa8ff1add02160d381f49e9
Juha Kukkonen <juha@juhaku.ddns.net>
2017-04-08 15:17:11 +0300

    wip joins

--- app/src/main/java/db/juhaku/juhakudb/filter/QuerProcessor.java
+++ app/src/main/java/db/juhaku/juhakudb/filter/QuerProcessor.java
@@ -1,15 +1,18 @@
 package db.juhaku.juhakudb.filter;
 
+import java.lang.reflect.Field;
 import java.util.HashMap;
 import java.util.Iterator;
 import java.util.Map;
 import java.util.concurrent.atomic.AtomicInteger;
 
+import db.juhaku.juhakudb.annotation.ManyToMany;
 import db.juhaku.juhakudb.core.NameResolver;
 import db.juhaku.juhakudb.core.schema.Schema;
 import db.juhaku.juhakudb.exception.NameResolveException;
 import db.juhaku.juhakudb.exception.QueryBuildException;
 import db.juhaku.juhakudb.filter.Root.Join;
+import db.juhaku.juhakudb.util.ReflectionUtils;
 import db.juhaku.juhakudb.util.StringUtils;
 
 /**
@@ -118,17 +121,8 @@ public class QuerProcessor {
              * If alias is empty and it does not exist in cache add new one.
              */
             if (StringUtils.isBlank(join.getAlias())) {
-                String alias = aliasMap.get(join.getModel());
 
-                if (StringUtils.isBlank(alias)) {
-                    alias = generateAlias(resolveTableName(join.getModel()));
-                    aliasMap.put(join.getModel(), alias);
-
-                    return alias;
-                } else {
-
-                    return alias;
-                }
+                return forModel(join.getModel());
             } else {
 
                 return join.getAlias();
@@ -158,7 +152,7 @@ public class QuerProcessor {
                 tableName = tableName.substring(index + 1);
             }
 
-            return aliasBuilder.toString();
+            return aliasBuilder.append(String.valueOf(count.incrementAndGet())).toString();
         }
 
         public static void clearCache() {
@@ -179,7 +173,7 @@ public class QuerProcessor {
 //
 //        left join person_books bp on p.id = pb.person_id left join books b on b.id = bp.book_id
 
-        void processJoins(Root<?> root, StringBuilder sql) {
+        void createJoins(Root<?> root, StringBuilder sql) {
             Class<?> model = root.getModel();
 
             String rootAlias = Alias.forModel(model);
@@ -188,14 +182,52 @@ public class QuerProcessor {
             for (Root<?> r : root.getJoins()) {
                 Join join = (Join) r;
 
+                Field targetField = ReflectionUtils.findField(model, join.getTarget());
+
+                if (targetField.isAnnotationPresent(ManyToMany.class)) {
+
+                } else {
+
+                }
+
                 sql.append(" ").append(join.getJoinMode().getValue()).append(" ")
                         .append(rootAlias).append(".").append(rootTable).append(" = ")
                         .append(Alias.forJoin(join)).append(".").append(resolveTableName(join.getModel()));
 
                 if (!join.getJoins().isEmpty()) {
-                    processJoins(join, sql);
+                    createJoins(join, sql);
                 }
             }
+        }
+
+        private void middleTableJoin(Root<?> root, StringBuilder sql) {
+
+        }
+
+        private void ownerJoin(Root<?> root, StringBuilder sql) {
+
+        }
+
+        /**
+         * Creates join sql with given join mode to target table according given parameters.
+         *
+         * @param sql Instance of {@link StringBuilder} where to append the sql.
+         * @param joinMode Instance of {@link JoinMode} that is being used with this join.
+         * @param toTable String name of the table that join is being made.
+         * @param fromColumn String name of the from join column.
+         * @param fromAlias String alias of from join column.
+         * @param toColumn String name of the to join column.
+         * @param toAlias String alias of to join column.
+         *
+         * @since
+         *
+         * @hide
+         */
+        private void joinSql(StringBuilder sql, JoinMode joinMode, String toTable, String fromColumn,
+                             String fromAlias, String toColumn, String toAlias) {
+            sql.append(" ").append(joinMode.getValue()).append(" ").append(toTable)
+                    .append(" ").append(toAlias).append(" ON ").append(fromAlias).append(".")
+                    .append(fromColumn).append(" = ").append(toAlias).append(".").append(toColumn);
         }
     }
 }
